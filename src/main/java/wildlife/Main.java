bdb02eff343c74615c5c07563c06efbc3350fbeb
Kariuki1976 <emmanuel.mwangi@student.moringaschool.com>
2022-05-18 16:30:48 +0300

    create wildlife package

--- src/main/java/wildlife/Main.java
+++ src/main/java/wildlife/Main.java
@@ -1,4 +1,10 @@
 package wildlife;
 
 public class Main {
+    static int getHerokuAssignedPort(){
+      ProcessBuilder processBuilder = new ProcessBuilder();
+      if(processBuilder.environment().get("PORT") !=null){
+          return Integer.parseInt((processBuilder.environment().get("PORT")));
+      }
+    }
 }
