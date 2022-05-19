490a393585797a2e0efee163d011084d0804cb0b
Kariuki1976 <emmanuel.mwangi@student.moringaschool.com>
2022-05-19 07:04:45 +0300

    create testclass  domain for Endagered animals

--- src/test/java/wildlife/domain/EndangeredTest.java
+++ src/test/java/wildlife/domain/EndangeredTest.java
@@ -1,2 +1,42 @@
-package wildlife.domain;public class EndangeredTest {
+package wildlife.domain;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EndangeredTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void endangeredAnimal_instantiatesCorrectly() {
+        Endangered endangered = new Endangered("monster","healthy","newborn");
+        assertTrue(endangered instanceof Endangered);
+    }
+
+    //can save into and read from database
+    @Test
+    void endangeredAnimal_instantiatesCorrectlyWithArguments() {
+        Endangered endangered = new Endangered("monster","healthy","12");
+        endangered.save();
+        System.out.println(Endangered.all().size());
+        assertEquals(endangered.getId(), Endangered.all().get(Endangered.all().size()-1).getId());
+    }
+
+    //can get item from database using id
+    @Test
+    void endangered_findById() {
+        Endangered endangered = new Endangered("monster","healthy","newborn");
+        endangered.save();
+        assertEquals(endangered,Endangered.searchById(endangered.getId()));
+    }
 }
